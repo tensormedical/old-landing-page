0df5902dce50c6fcd461f5f65b904bbb1b14e2dd
Pol Valverde i Valverde <hello@polvalverde.com>
2019-01-01 15:50:48 +0100

    Random backgrounds
    Applied a random background selector, that switches between black&white and white&black

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,6 @@
 // tensormedical.ai Landing Page TS Script
-
 import $ from 'jquery';
+import 'particles.js';
 
 init();
 
@@ -12,5 +12,30 @@ function init() : void {
   
   $('#title1').css("font-size", ns);
   $('.central').css("font-size", ns);
+
+  let bool : number = Math.random();
+
+  if (bool < 0.5){
+    //@ts-ignore
+    particlesJS.load('background', 'js/assets/config.json', () => {
+      console.log('callback - particles.js config loaded');
+    });
+    // @ts-ignore
+    particlesJS.load('background2', 'js/assets/config2.json', () => {
+      console.log('callback - particles.js config loaded');
+    });
+  } else {
+    $('html').css('background', 'black');
+    $('.container').css('color', 'white')
+    $('.mail a').css('color', 'white');
+    //@ts-ignore
+    particlesJS.load('background', 'js/assets/config_b.json', () => {
+      console.log('callback - particles.js config loaded');
+    });
+    // @ts-ignore
+    particlesJS.load('background2', 'js/assets/config2_b.json', () => {
+      console.log('callback - particles.js config loaded');
+    });
+  } 
 
 }
